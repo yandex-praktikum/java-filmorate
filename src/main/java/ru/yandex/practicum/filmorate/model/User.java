eecb331df3da726f19043b64aae7b55b5cdd0d63
Eldar <eldar.gai@yandex.ru>
2022-05-27 11:46:06 +0500

    add field 'friends'

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,12 +3,14 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 
 @Data
 public class User {
-    private long id;
+    private Long id;
     private final String email;
     private final String login;
     private String name;
     private final LocalDate birthday;
+    private HashSet<Long> friends = new HashSet<>();
 }
