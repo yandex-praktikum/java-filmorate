c0f0458a3041fe2f37e9dcbac5fbe16968ae4f68
Eldar <eldar.gai@yandex.ru>
2022-05-27 11:49:08 +0500

    add class ErrorResponse

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ErrorResponse {
+    private final String error;
+    private final String description;
+
+    public ErrorResponse(String error, String description) {
+        this.error = error;
+        this.description = description;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
