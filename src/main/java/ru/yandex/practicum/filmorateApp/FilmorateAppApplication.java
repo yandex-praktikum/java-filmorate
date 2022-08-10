396b86602548c9c9b0a3fa4ad7f569e3a5f50c24
Rodion <rgsystem@list.ru>
2022-08-11 02:13:46 +0500

    init project

---
+++ src/main/java/ru/yandex/practicum/filmorateApp/FilmorateAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorateApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmorateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmorateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorateApp/FilmorateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
