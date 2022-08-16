31a4bff6afc2656c032cb477a7fc919eac40193e
Rodion <rgsystem@list.ru>
2022-08-16 14:47:59 +0500

    branch comtrollers-films-users

--- src/main/java/controllers/FilmController.java
+++ src/main/java/controllers/FilmController.java
@@ -1,12 +1,8 @@
 package controllers;
 
 import jakarta.validation.Valid;
-import lombok.Data;
-
 import lombok.extern.slf4j.Slf4j;
 import model.Film;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,19 +16,21 @@ public class FilmController {
     private List<Film> films = new ArrayList<>();
 
     @PostMapping
-    public Film addFilm(@Valid @RequestBody Film film){
+    public Film addFilm(@Valid @RequestBody Film film) {
         films.add(film);
+        log.info("add film");
         return film;
     }
 
     @PatchMapping
-    public Film updateFilm(@Valid @RequestBody Film film){
+    public Film updateFilm(@Valid @RequestBody Film film) {
         films.add(film);
+        log.info("update film");
         return film;
     }
 
     @GetMapping
-    public List<Film> getAllFilms(){
+    public List<Film> getAllFilms() {
         return films;
     }
 }
--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -2,10 +2,7 @@ package controllers;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import model.Film;
 import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,23 +12,24 @@ import java.util.List;
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-// branch controllers-films-users
- private List<User> users = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
         users.add(user);
+        log.info("add user");
         return user;
     }
 
     @PatchMapping
     public User updateUser(@Valid @RequestBody User user) {
         users.add(user);
+        log.info("update user");
         return user;
     }
 
     @GetMapping
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return users;
     }
 }
