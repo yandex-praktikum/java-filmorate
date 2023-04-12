44d651fd6a18d3d81f378b5cd9aad72462fc1222
Sirotin Alexandr <an9262778931@yandex.ru>
2023-04-12 08:22:01 +0200

    Add tests and fix mistakes

--- src/main/java/ru/yandex/practicum/filmorate/exception/ApiError.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ApiError.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.filmorate.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiError {
     private String message;
