f1f462075f7d88d2fa9d9e7eb44076ab981de5b8
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-27 18:24:20 +0300

    Create FilmController.java

---
+++ src/main/java/ru/yandex/practicum/controller/FilmController.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.exeptions.FilmAlreadyExistException;
+import ru.yandex.practicum.exeptions.InvalidIdException;
+import ru.yandex.practicum.model.Film;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class FilmController {
+
+    private List<Film> films = new ArrayList<>();
+
+    @GetMapping("/films")
+    public List<Film> findAll() {
+        return films;
+    }
+
+    @PostMapping("/film")
+    public Film create(@RequestBody Film film) throws FilmAlreadyExistException {
+        if (films.contains(film)) {
+            throw new FilmAlreadyExistException();
+
+        }
+        else {
+            films.add(film);
+            return film;
+        }
+    }
+
+    @PutMapping("/film")
+    public Film update(@RequestBody Film film) throws InvalidIdException {
+        if (film.getId() == null || film.getId() < 1) {
+            throw new InvalidIdException();
+        }
+        films.add(film);
+        return film;
+    }
+}
