746a13f45625c7567e61bfed84561b1563141180
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-27 18:44:21 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/controller/FilmController.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.exeptions.FilmAlreadyExistException;
-import ru.yandex.practicum.exeptions.InvalidIdException;
+import ru.yandex.practicum.exeptions.InvalidFilmeException;
 import ru.yandex.practicum.model.Film;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +20,21 @@ public class FilmController {
     }
 
     @PostMapping("/film")
-    public Film create(@RequestBody Film film) throws FilmAlreadyExistException {
+    public Film create(@RequestBody Film film) throws InvalidFilmeException {
         if (films.contains(film)) {
-            throw new FilmAlreadyExistException();
+            throw new InvalidFilmeException("Фильм уже есть в коллекции!");
+        }
+        else if (film.getName().isEmpty()) {
+            throw new InvalidFilmeException("Название фильма не может быть пустым!");
+        }
+        else if (film.getDescription().length() > 200) {
+            throw new InvalidFilmeException("Длинна описания превышает 200 символов!");
+        }
+        else if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new InvalidFilmeException("Дата релиза не должна быть раньше 28 декабря 1895 года!");
+        }
+        else if (film.getDuration().getSeconds() < 0) {
+            throw new InvalidFilmeException("Продолжительность фильма должна быть положительной!");
         }
         else {
             films.add(film);
@@ -30,9 +43,9 @@ public class FilmController {
     }
 
     @PutMapping("/film")
-    public Film update(@RequestBody Film film) throws InvalidIdException {
+    public Film update(@RequestBody Film film) throws InvalidFilmeException {
         if (film.getId() == null || film.getId() < 1) {
-            throw new InvalidIdException();
+            throw new InvalidFilmeException();
         }
         films.add(film);
         return film;
