551581ebf95a0ba799e5074eb9d993bd3a011fa5
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 13:28:25 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorete/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorete/controller/UserController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorete.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorete.exeptions.InvalidEmailException;
-import ru.yandex.practicum.filmorete.exeptions.UserAlreadyExistException;
+import ru.yandex.practicum.filmorete.exeptions.ValidationUserException;
 import ru.yandex.practicum.filmorete.model.User;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,19 +14,21 @@ public class UserController {
 
     private Map<String, User> users = new HashMap<>();
 
+    private Integer lastIdentification = 1;
+
     @GetMapping("/users")
     public Collection<User> findAll() {
         return users.values();
     }
 
     @PostMapping("/user")
-    public User create(@RequestBody User user) throws UserAlreadyExistException, InvalidEmailException {
+    public User create(@RequestBody User user) throws ValidationUserException {
         if (users.containsKey(user.getEmail())) {
-            throw new UserAlreadyExistException();
+            throw new ValidationUserException();
 
         }
         else if (user.getEmail() == null || user.getEmail().equals("")) {
-            throw new InvalidEmailException();
+            throw new ValidationUserException();
         }
         else {
             users.put(user.getEmail(), user);
@@ -35,11 +37,36 @@ public class UserController {
     }
 
     @PutMapping("/user")
-    public User update(@RequestBody User user) throws InvalidEmailException {
+    public User update(@RequestBody User user) throws ValidationUserException {
         if (user.getEmail() == null || user.getEmail().equals("")) {
-            throw new InvalidEmailException();
+            throw new ValidationUserException();
         }
         users.put(user.getEmail(), user);
         return user;
+    }
+
+    private void validatorUser(User user) throws ValidationUserException {
+        if (user.getEmail().isEmpty()) {
+            throw new ValidationUserException("Название фильма не может быть пустым!");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationUserException("Электронная почта должна содержать символ '@'!");
+        }
+        if (user.getLogin().isEmpty()) {
+            throw new ValidationUserException("Логин не может быть пустым!");
+        }
+        if (user.getLogin().contains(" ")) {
+            throw new ValidationUserException("Логин не должен содержать пробелы!");
+        }
+        if (user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationUserException("Дата рождения не должна быть в будущем!");
+        }
+    }
+
+    private Integer getLastIdentification() {
+        return lastIdentification++;
     }
 }
