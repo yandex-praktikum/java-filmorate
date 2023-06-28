79acc420ff53fd43c845929b057b2a1bba643ad1
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 13:43:54 +0300

    Update ValidationFilmException.java

--- src/main/java/ru/yandex/practicum/filmorete/exeptions/ValidationFilmException.java
+++ src/main/java/ru/yandex/practicum/filmorete/exeptions/ValidationFilmException.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorete.exeptions;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ValidationFilmException extends Throwable {
 
     public ValidationFilmException() {
@@ -8,5 +11,6 @@ public class ValidationFilmException extends Throwable {
 
     public ValidationFilmException(String message) {
         super(message);
+        log.debug(message);
     }
 }
