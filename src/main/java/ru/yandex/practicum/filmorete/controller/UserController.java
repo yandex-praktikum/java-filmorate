396a991570994e66c21b63e23e32b85363af8874
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 12:41:24 +0300

    Create UserController.java

---
+++ src/main/java/ru/yandex/practicum/filmorete/controller/UserController.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorete.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorete.exeptions.InvalidEmailException;
+import ru.yandex.practicum.filmorete.exeptions.UserAlreadyExistException;
+import ru.yandex.practicum.filmorete.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    private Map<String, User> users = new HashMap<>();
+
+    @GetMapping("/users")
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user) throws UserAlreadyExistException, InvalidEmailException {
+        if (users.containsKey(user.getEmail())) {
+            throw new UserAlreadyExistException();
+
+        }
+        else if (user.getEmail() == null || user.getEmail().equals("")) {
+            throw new InvalidEmailException();
+        }
+        else {
+            users.put(user.getEmail(), user);
+            return user;
+        }
+    }
+
+    @PutMapping("/user")
+    public User update(@RequestBody User user) throws InvalidEmailException {
+        if (user.getEmail() == null || user.getEmail().equals("")) {
+            throw new InvalidEmailException();
+        }
+        users.put(user.getEmail(), user);
+        return user;
+    }
+}
