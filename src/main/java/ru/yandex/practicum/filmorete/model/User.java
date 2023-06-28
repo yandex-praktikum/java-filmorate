f259e916d07a24e596d7d2ffe8cd941e124c2ebb
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 15:26:36 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorete/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorete/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorete.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.time.LocalDate;
 
@@ -12,15 +11,11 @@ public class User {
 
     private Integer id;
 
-    @NonNull
     private String name;
 
-    @NonNull
     private LocalDate birthday;
 
-    @NonNull
     private String login;
 
-    @NonNull
     private String email;
 }
