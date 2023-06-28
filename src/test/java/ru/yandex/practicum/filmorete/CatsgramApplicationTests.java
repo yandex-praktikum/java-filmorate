b198340a4e506d80ff7f675329ad9fc7e204ac5c
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 12:42:10 +0300

    Create CatsgramApplicationTests.java

---
+++ src/test/java/ru/yandex/practicum/filmorete/CatsgramApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
