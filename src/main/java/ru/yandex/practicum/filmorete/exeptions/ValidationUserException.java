84b022eaad78be358359f0852934283c979ce30a
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 13:43:57 +0300

    Update ValidationUserException.java

--- src/main/java/ru/yandex/practicum/filmorete/exeptions/ValidationUserException.java
+++ src/main/java/ru/yandex/practicum/filmorete/exeptions/ValidationUserException.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorete.exeptions;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ValidationUserException extends Throwable {
 
-    public ValidationUserException() {
-    }
+    public ValidationUserException() {}
 
     public ValidationUserException(String message) {
         super(message);
+        log.debug(message);
     }
 }
