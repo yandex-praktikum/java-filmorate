bef993ef1c2e71830655f6c993a765dabfbf2074
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2023-06-28 16:11:32 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorete/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorete/controller/FilmController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorete.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorete.exeptions.ValidationFilmException;
+import ru.yandex.practicum.filmorete.exeptions.ValidationUserException;
 import ru.yandex.practicum.filmorete.model.Film;
 
 import java.time.LocalDate;
@@ -43,28 +44,35 @@ public class FilmController {
         if (film.getId() == null || film.getId() < 1) {
             throw new ValidationFilmException("Не указан ID фильма!");
         }
-        else if (!films.containsKey(film.getId())) {
-            throw new ValidationFilmException("Фильм не найден для обновления!");
-        }
-        else {
-            validatorFilms(film);
-            films.put(film.getId(), film);
-            log.debug("Обновление фильма: {}", film.getName());
-            return film;
-        }
+        validatorFilms(film);
+        films.put(film.getId(), film);
+        log.debug("Обновление фильма: {}", film.getName());
+        return film;
     }
 
     private void validatorFilms(Film film) throws ValidationFilmException {
-        if (film.getName().isEmpty()) {
-            throw new ValidationFilmException("Название фильма не может быть пустым!");
+        if (!films.containsKey(film.getId())) {
+            throw new ValidationFilmException("Фильм не найден для обновления!");
+        }
+        if (film.getName() == null || film.getName().isEmpty()) {
+            throw new ValidationFilmException("Имя фильма равно null или отсутствует!");
+        }
+        if (film.getDescription() == null) {
+            throw new ValidationFilmException("Описание фильма равно null или отсутствует!");
         }
         if (film.getDescription().length() > 200) {
             throw new ValidationFilmException("Длинна описания превышает 200 символов!");
         }
+        if (film.getReleaseDate() == null) {
+            throw new ValidationFilmException("Дата релиза фильма равно null или отсутствует!");
+        }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             throw new ValidationFilmException("Дата релиза не должна быть раньше 28 декабря 1895 года!");
         }
-        if (film.getDuration() < 0) {
+        if (film.getDuration() == null) {
+            throw new ValidationFilmException("Продолжительность фильма равно null или отсутствует!");
+        }
+        if (film.getDuration() < 1) {
             throw new ValidationFilmException("Продолжительность фильма должна быть положительной!");
         }
     }
